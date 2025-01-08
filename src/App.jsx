06b0fe5d95736b82da32b7ea530f6b24e91d254c
sar9494 <batbayarsaruul0@gmail.com>
2025-01-08 20:16:45 +0800

    dfsaf

--- src/App.jsx
+++ src/App.jsx
@@ -5,36 +5,31 @@ import moment from "moment";
 function App() {
   const [inputValue, setInputValue] = useState("");
   const [todos, setTodos] = useState([]);
-  const [mainToDo, setMainToDo] = useState([]);
+  const [logs, setLogs] = useState([]);
   const [filterState, setFilterState] = useState("all");
-  const [logs, setLog] = useState([]);
 
   const createNewTask = () => {
     if (inputValue.length === 0) {
     } else {
       const newTask = {
         task: inputValue,
-        state: "active",
+        status: "active",
         id: uuidv4(),
+      };
+
+      const newLog = {
+        task: newTask.task,
+        status: newTask.status,
+        id: newTask.id,
         logs: [
           {
             status: "active",
-            time: addNewLog(),
+            time: addTime(),
           },
         ],
       };
       setTodos([...todos, newTask]);
-      setMainToDo([...mainToDo, newTask]);
-      setLog([
-        ...logs,
-        [
-          {
-            taskDescription: newTask.task,
-            id: newTask.id,
-            logs: [{ status: "ACTIVE", time: addNewLog() }],
-          },
-        ],
-      ]);
+      setLogs([...logs, newLog]);
       setInputValue("");
     }
   };
@@ -44,23 +39,39 @@ function App() {
       if (task.id == id) {
         console.log({
           ...task,
-          state: task.state == "active" ? "completed" : "active",
+          status: task.status == "active" ? "completed" : "active",
+        });
+        return {
+          ...task,
+          status: task.status == "active" ? "completed" : "active",
+        };
+      } else {
+        return task;
+      }
+    });
+    setTodos(updatedTodos);
+
+    const updatedlogss = logs.map((task) => {
+      if (task.id == id) {
+        console.log({
+          ...task,
+          status: task.status == "active" ? "completed" : "active",
           logs: [
             ...task.logs,
             {
-              status: task.state == "active" ? "completed" : "active",
-              time: addNewLog(),
+              status: task.status == "active" ? "completed" : "active",
+              time: addTime(),
             },
           ],
         });
         return {
           ...task,
-          state: task.state == "active" ? "completed" : "active",
+          status: task.status == "active" ? "completed" : "active",
           logs: [
             ...task.logs,
             {
-              status: task.state == "active" ? "completed" : "active",
-              time: addNewLog(),
+              status: task.status == "active" ? "completed" : "active",
+              time: addTime(),
             },
           ],
         };
@@ -68,89 +79,50 @@ function App() {
         return task;
       }
     });
-    setTodos(updatedTodos);
-    console.log(updatedTodos);
-    setLog([
-      ...logs,
-      [
-        {
-          taskDescription: updatedTodos.task,
-          id: updatedTodos.id,
-          logs: [{ status:updatedTodos.state, time: addNewLog() }],
-        },
-      ],
-    ])
-    setMainToDo(updatedTodos)
-    
+    setLogs(updatedlogss);
   };
 
-  
-
   const deleteToDo = (id) => {
-    const updatedTodo = todos.filter((task) => {
+    const updatedTodo = logs.map((task) => {
       if (task.id == id) {
-        console.log({
-          ...task,
-          state: (task.state = "deleted"),
-          logs: [
-            ...task.logs,
-            {
-              status:"deleted",
-              time: addNewLog(),
-            },
-          ],
-        });
-
         return {
           ...task,
-          state: (task.state = "deleted"),
+          status: (task.status = "deleted"),
           logs: [
             ...task.logs,
             {
-              status: task.state = "deleted",
-              time: addNewLog(),
+              status: (task.status = "deleted"),
+              time: addTime(),
             },
           ],
         };
-      }
+      } else return task;
     });
-    console.log(updatedTodo);
-    setLog([
-      ...logs,
-      [
-        {
-          taskDescription: updatedTodo.task,
-          id: updatedTodo.id,
-          logs: [{ status:updatedTodo.state, time: addNewLog() }],
-        },
-      ],
-    ])
-    setMainToDo(updatedTodo)
+    setLogs(updatedTodo);
+
     const updatedTodos = todos.filter((task) => task.id !== id);
     setTodos(updatedTodos);
-
-    setMainToDo(updatedTodos)
   };
-  console.log(mainToDo)
+
   const clearCompleted = () => {
-    // const updatedMainTodos = mainToDo.map((todo)=>{
-    //   if(todo.state=="completed"){
-    //     console.log({
-    //       ...todo,state:(todo.state="deleted"),logs:[...todo.logs,{status:"deleted",time:addNewLog()}]
-    //     });
-        
-    //     return {
-    //       ...todo,state:(todo.state="deleted"),logs:[...todo.logs,{status:"deleted",time:addNewLog()}]
-    //     }
-    //   }
-    //   else return todo
-    // })
-    // setMainToDo(updatedMainTodos)
-    const updatedTodos = todos.filter((task) => task.state !== "completed");
+    const updatedlogss = logs.map((log) => {
+      if (log.status == "completed") {
+        return {
+          ...log,
+          status: (log.status = "completed and deleted"),
+          logs: [
+            ...log.logs,
+            { status: "completed and deleted", time: addTime() },
+          ],
+        };
+      } else return log;
+    });
+    setLogs(updatedlogss);
+
+    const updatedTodos = todos.filter((task) => task.status !== "completed");
     setTodos(updatedTodos);
   };
 
-  console.log(mainToDo)
   const filterButtonHandler = (filterState) => {
     setFilterState(filterState);
   };
@@ -159,11 +131,34 @@ function App() {
     setInputValue(e.target.value);
   };
 
-  const addNewLog = () => {
-    return moment().format("MMMM Do YYYY, h:mm:ss a");
+  const addTime = () => {
+    return moment().format("llll");
   };
+
+  const restoreToDo = (id) => {
+    const updatedLogs = logs.map((log) => {
+      if (log.id == id) {
+        setTodos([
+          ...todos,
+          {
+            task: log.task,
+            status: "active",
+            id: id,
+          },
+        ]);
+        console.log(log);
+        return {
+          ...log,
+          status: "active",
+          logs: [...log.logs, { status: "active", time: addTime() }],
+        };
+      } else return log;
+    });
+    setFilterState("logRemove");
+    setLogs(updatedLogs);
+  };
+
   return (
-    <div className="app">
       <div className="container">
         <h2>To-Do List</h2>
         <div className="addTask">
@@ -171,7 +166,6 @@ function App() {
             type="text"
             placeholder="Add a new task..."
             value={inputValue}
-            className="textInput"
             onChange={handleInputChange}
           />
           <button className="addButton" onClick={createNewTask}>
@@ -207,99 +201,101 @@ function App() {
             onClick={() => {
               filterButtonHandler("log");
             }}
-            className={filterState == "log" ? "clicked" : "nonClicked"}
+            className={filterState.includes("log") ? "clicked" : "nonClicked"}
           >
             Logs
           </button>
         </div>
         <div className="logs">
-          {mainToDo.map(
+          {logs.map(
             (todo, index) =>
-              filterState == "log" && (
-                <div key={index} style={{display:"flex",width:"98%",justifyContent:"center",
-                  flexDirection:"column"
-                }}>
-                  <div style={{ display: "flex",width:"95%",justifyContent:"space-between"}}>
-                    <h4>{todo.task}</h4>
-                    <p style={{width:"250px",wordWrap:"break-word",color:"green"}}>{todo.id}</p>
-                  </div>
+              filterState.includes("log") && (
+                <div key={index}>
+                  <h4>Task description: {todo.task}</h4>
                   <ul>
-                    {todo.logs.map((log,index) => (
-                      <li key={index} style={{display:"flex", justifyContent:"space-between", width:"95%"}}>
-                        <p>STATUS:{log.status}</p> <p>TIME:{log.time}</p>
+                    {todo.logs.map((log, index) => (
+                      <li
+                        key={index}
+                        style={{
+                          color: log.status.includes("deleted")
+                            ? "red"
+                            : "black",
+                        }}
+                      >
+                        <p>Status: {log.status}</p>{" "}
+                        <p>Time: {"  " + log.time}</p>
                       </li>
                     ))}
                   </ul>
+                  <button
+                    className={"clicked"}
+                    style={{
+                      display: todo.status.includes("deleted")
+                        ? "block"
+                        : "none",
+                    }}
+                    onClick={() => restoreToDo(todo.id)}
+                  >
+                    Restore
+                  </button>
                 </div>
               )
           )}
         </div>
-        {todos.length === 0 &&filterState!="log" ? (
-          <p style={{ color: "#71717a" }}>No tasks yet. Add one above!</p>
+        {todos.length === 0 && filterState == "all" ? (
+          (<p style={{ color: "#71717a" }}>No tasks yet. Add one above!</p>)
         ) : (
           <div className="todos">
             {todos
               .filter((todo) => {
                 if (filterState == "active") {
-                  return todo.state === "active";
+                  return todo.status === "active";
                 } else if (filterState == "completed") {
-                  return todo.state === "completed";
+                  return todo.status === "completed";
                 } else if (filterState === "all") {
                   return true;
                 }
               })
               .map((task, index) => (
                 <label key={index} className="todo">
-                  <div
-                    style={{
-                      display: "flex",
-                      alignItems: "center",
-                      gap: "10px",
-                    }}
-                  >
+                  <div>
                     <input
                       type="checkbox"
                       style={{
                         height: "fit-content",
                       }}
                       onChange={() => checkboxHandler(task.id)}
-                      checked={task.state == "completed"}
+                      checked={task.status == "completed"}
                     />
                     <p
                       style={{
                         textDecoration:
-                          task.state == "completed" && "line-through",
-                        fontWeight: "300",
-                        color:task.state == "completed" && "red",
-                        width: "245px",
-                        wordWrap: "break-word",
+                          task.status == "completed" && "line-through",
+                        color: task.status == "completed" && "red",
                       }}
                     >
                       {task.task}
                     </p>
                   </div>
                   <button
-                    style={{ color: "red" }}
                     onClick={() => deleteToDo(task.id)}
+                    className="deleteButton"
                   >
                     Delete
                   </button>
                 </label>
               ))}
+              <p style={{display:(filterState=="completed"||filterState=="active")&&todos.filter((todo)=>todo.status==filterState).length==0?"block":"none",
+                color: "#71717a"
+              }}>No {filterState} tasks found.</p>
             <div
-              style={{
-                display: "flex",
-                justifyContent: "space-between",
-                height: "60px",
-                alignItems: "center",
-                width: "345px",
-                color: "#6B7280",
-              }}
+              className="clearCompleted"
+              style={{ display: filterState != "log" &&todos.length!=0 ? "flex" : "none" }}
             >
               <p>
                 {
                   todos.filter((todo) => {
-                    if (todo.state == "completed") {
+                    if (todo.status == "completed") {
                       return true;
                     }
                   }).length
@@ -317,7 +313,6 @@ function App() {
           <a href="https://pinecone.mn/">Pinecone Academy</a>
         </div>
       </div>
-    </div>
   );
 }
 
